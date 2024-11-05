866b25284cfdafdc2e64983613b266b13ec6836c
s230176 <gimpo.s230176@ggh.goe.go.kr>
2024-11-05 15:40:53 +0900

    Update test2.py

--- test2.py
+++ test2.py
@@ -18,10 +18,10 @@ st.write(' - 인공지능 모델 정확도 : ***')
 col1, col2, col3 = st.columns(3)  
 with col1:
       st.subheader('데이터시각화1')
-      st.image('시각화1.png' )   # 이미지 불러오기
+      st.image('시각화1 (1).png' )   # 이미지 불러오기
 with col2:
       st.subheader('데이터시각화2')
-      st.image('시각화2.png' )   # 이미지 불러오기
+      st.image('시각화2 (2).png' )   # 이미지 불러오기
 with col3:
       st.subheader('데이터시각화3')
       st.image('시각화3.png')   # 이미지 불러오기
